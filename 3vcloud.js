68aae5278de20088d8af0505955166b25d49a7c6
Jon Riley <jon@3vcloud.uk>
2015-11-05 17:33:27 +0000

    Parallax code

--- 3vcloud.js
+++ 3vcloud.js
@@ -7,6 +7,22 @@ var Page = {
 	mandrilla_api_key:'GmD13qOjRM46o-EuQhLNjQ',
 	contact_email:"jon.r@live.com",
 	contact_name:"Jon",
+	skills:[{
+		title:'PHP',
+		image:'images/php.png'
+	},{
+		title:'HTML5',
+		image:'images/html5.png'
+	},{
+		title:'Microsoft Azure',
+		image:'images/azure.png'
+	},{
+		title:'Google Cloud',
+		image:'images/googlecloud.png'
+	},{
+		title:'CSS3',
+		image:'images/css3.png'
+	}],
 	success:function(message) {
 		
 	},
@@ -108,4 +124,63 @@ var ContactForm = {
 $(function()
 {
 	$("#contactFormSubmitButton").on('click',function(){ContactForm.send()});
+	init();
 });
+
+function init()
+{
+	layoutSkills();
+
+}
+
+var platforms = [],
+    platformSize = 20,
+    platformWidth = 200,
+    platformHeight = 200;
+
+function generatePlatforms(k) {
+
+}
+
+function layoutSkills()
+{
+	//var css='<style>';
+	var skill_divs = '';
+	
+	$(".skills_container").html("<div id='skill_container_dummy' class='skill_container' style='visibility:hidden'>");
+	var platformHeight = parseInt($("#skill_container_dummy").height());
+	var platformWidth = parseInt($("#skill_container_dummy").width());
+	var container_margin = parseInt($("#skill_container_dummy").css('margin'));
+	var placed = 0,
+		maxAttempts = Page.skills.length*10;
+	var platforms=[];
+	var platformSize = 20;
+	
+	while(placed < Page.skills.length && maxAttempts > 0) {
+		var x = Math.floor(Math.random()*platformWidth),
+			y = Math.floor(Math.random()*platformHeight),
+			available = true;
+		for(var point in platforms) {
+		  if(Math.abs(point.x-x) < platformSize && Math.abs(point.y-y) < platformSize) {
+			available = false;
+			break;
+		  }
+		}
+		if(available) {
+		  platforms.push({
+			x: x,
+			y: y
+		  });
+		  placed += 1;
+		}
+		maxAttempts -= 1;
+	}
+	for(var i = 0;i < Page.skills.length; i++)
+	{
+		skill_divs += "<div class='skill_container' style='left:"+platforms[i].x+";top:"+platforms[i].y+";background-image:url("+Page.skills[i].image+")'>"+Page.skills[i].title+"</div>";
+	}
+	//css+='</style>';
+	//$('head').append(css);
+	$(".skills_container").html(skill_divs);
+
+}
